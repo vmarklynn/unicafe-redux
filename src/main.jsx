5e3974cc2498fca14eff36e27ca46e0c84e5eb89
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-07-31 11:38:05 -0700

    Fixed reducer and tests; initiated vitest

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button>ok</button>
+      <button>bad</button>
+      <button>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok</div>
+      <div>bad</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
